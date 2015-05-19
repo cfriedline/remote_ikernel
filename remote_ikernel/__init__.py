7ac8a38c5e9692fee316c76809048306bbac72df
tdd20 <tdd20@cam.ac.uk>
2015-05-19 11:18:47 +0100

    increment minor release

--- remote_ikernel/__init__.py
+++ remote_ikernel/__init__.py
@@ -5,5 +5,5 @@ Run kernels remotely for IPython/Jupyter notebooks.
 
 """
 
-__version__ = '0.2'
+__version__ = '0.2.1'
 RIK_PREFIX = 'rik_'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='remote_ikernel',
-      version='0.2',
+      version='0.2.1',
       description='Running IPython kernels through batch queues',
       long_description=open('README.rst').read(),
       author='Tom Daff',
